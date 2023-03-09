f4c16375f9b93c6359e794f62c82786784faffeb
ibu <ibumohamed2001@gmail.com>
2023-03-09 19:50:15 +0530

    second

--- consumer/index.js
+++ consumer/index.js
@@ -1,11 +1,223 @@
-// const express = require('express');
 const amqp = require("amqplib");
 
-// const app = express();
-// const PORT = 3000;
 
-// console.log("ssss");
-// Define an async function that creates a connection to RabbitMQ and creates a channel
+// async function createChannel() {
+//   try {
+//     // Connect to RabbitMQ server
+//     const connection = await amqp.connect("amqp://localhost");
+
+//     // Create a channel
+//     const channel = await connection.createChannel();
+//     const result = await channel.assertQueue("task", { durable: true });
+//     channel.prefetch(1);
+//
+//     channel.consume(
+//       "task",
+//       (message) => {
+//         // console.log(message.content.toString().split("").length);
+//         const sec = message.content.toString().split(".").length - 1;
+//         console.log("task recived", message.content.toString());
+//         setTimeout(() => {
+//           console.log("process finished");
+//           channel.ack(message);
+//         }, sec * 1000);
+//       },
+//       { noAck: false }
+//     );
+
+//     console.log("Channel created successfully");
+//   } catch (error) {
+//     console.error("Error creating channel:", error);
+//   }
+// }
+
+//fanout exchange
+// const exchangeName = "logs";
+// async function createChannel() {
+//   try {
+//     // Connect to RabbitMQ server
+//     const connection = await amqp.connect("amqp://localhost");
+
+//     // Create a channel
+//     const channel = await connection.createChannel();
+//     const result = await channel.assertExchange(exchangeName, "fanout", {
+//       durable: false,
+//     });
+
+//     const q = await channel.assertQueue("", { exclusive: true });
+//     console.log("waitong for messafe in queuq", q.queue);
+//     channel.bindQueue(q.queue, exchangeName, "");
+
+//     channel.consume(
+//       q.queue,
+//       (message) => {
+//         if (message.content) {
+//           console.log("the message is", message.content.toString());
+//         }
+//       },
+//       { noAck: true }
+//     );
+
+//     console.log("Channel created successfully");
+//   } catch (error) {
+//     console.error("Error creating channel:", error);
+//   }
+// }
+
+//  direct exchange
+// const args = process.argv.slice(2);
+// const exchangeName = "direct_logs";
+
+// if (args.length === 0) {
+//   console.log("please feed this [error] [warning] [info]");
+//   process.exit(1);
+// }
+// async function createChannel() {
+//   try {
+//     // Connect to RabbitMQ server
+//     const connection = await amqp.connect("amqp://localhost");
+
+//     // Create a channel
+//     const channel = await connection.createChannel();
+//     const result = await channel.assertExchange(exchangeName, "direct", {
+//       durable: false,
+//     });
+
+//     const q = await channel.assertQueue("", { exclusive: true });
+
+//     console.log("waitong for messafe in queuq", q.queue);
+//     args.forEach(function (sevrity) {
+//       channel.bindQueue(q.queue, exchangeName, sevrity);
+//     });
+
+//     channel.consume(
+//       q.queue,
+//       (message) => {
+//         if (message.content) {
+//           console.log(
+//             "routing key :",
+//             message.fields.routingKey,
+//             "content : ",
+//             message.content.toString()
+//           );
+//         }
+//       },
+//       { noAck: true }
+//     );
+
+//     console.log("Channel created successfully");
+//   } catch (error) {
+//     console.error("Error creating channel:", error);
+//   }
+// }
+
+//  topic exchange
+// const args = process.argv.slice(2);
+// const exchangeName = "topic_logs";
+
+// if (args.length === 0) {
+//   console.log("please feed this [error] [warning] [info]");
+//   process.exit(1);
+// }
+// async function createChannel() {
+//   try {
+//     // Connect to RabbitMQ server
+//     const connection = await amqp.connect("amqp://localhost");
+
+//     // Create a channel
+//     const channel = await connection.createChannel();
+//     const result = await channel.assertExchange(exchangeName, "topic", {
+//       durable: false,
+//     });
+
+//     const q = await channel.assertQueue("", { exclusive: true });
+
+//     console.log("waitong for messafe in queuq", q.queue);
+//     args.forEach(function (sevrity) {
+//       channel.bindQueue(q.queue, exchangeName, sevrity);
+//     });
+
+//     channel.consume(
+//       q.queue,
+//       (message) => {
+//         if (message.content) {
+//           console.log(
+//             "routing key :",
+//             message.fields.routingKey,
+//             "content : ",
+//             message.content.toString()
+//           );
+//         }
+//       },
+//       { noAck: true }
+//     );
+
+//     console.log("Channel created successfully");
+//   } catch (error) {
+//     console.error("Error creating channel:", error);
+//   }
+// }
+
+// header exchange
+
+// const exchangeName = "header_logs";
+// const args = process.argv.slice(2);
+// const type = args[0] || "any";
+// // console.log("type");
+// async function createChannel() {
+//   try {
+//     // Connect to RabbitMQ server
+//     const connection = await amqp.connect("amqp://localhost");
+
+//     // Create a channel
+//     const channel = await connection.createChannel();
+//     const result = await channel.assertExchange(exchangeName, "headers", {
+//       durable: false,
+//     });
+
+//     const q = await channel.assertQueue("", { exclusive: true });
+
+//     console.log("waitong for messafe in queuq", q.queue);
+
+//     channel.bindQueue(q.queue, exchangeName, "", {
+//       "account": "new", "method": "google", "x-match": type
+    
+      
+//     });
+
+//     channel.consume(
+//       q.queue,
+//       (message) => {
+//         if (message.content) {
+//           console.log(
+//             "/header:",
+//             message.properties.headers,
+//             "/content : ",
+//             message.content.toString()
+//           );
+//         }
+//       },
+//       { noAck: true }
+//     );
+
+//     console.log("Channel created successfully");
+//   } catch (error) {
+//     console.error("Error creating channel:", error);
+//   }
+// }
+
+//  example for rpc-remote procedural protocall
+
+
+
+const queueName="rpc_queue"
+function fibonacci(n){
+  if(n==0||n==1){
+    return n
+  }else{
+    return fibonacci(n-1)+fibonacci(n-2);
+  }
+}
 async function createChannel() {
   try {
     // Connect to RabbitMQ server
@@ -13,22 +225,29 @@ async function createChannel() {
 
     // Create a channel
     const channel = await connection.createChannel();
-    const result = await channel.assertQueue("task", { durable: true });
-    // channel.sendToQueue("jobs", Buffer.from(JSON.stringify(message)));
-    // onsole.log("job sent succesfullt", message.number);
-    channel.consume("task", (message) => {
-      // console.log(message.content.toString().split("").length);
-      const sec = message.content.toString().split(".").length - 1;
-      console.log("task recived", message.content.toString());
-      setTimeout(() => {
-        console.log("process finished");
-      }, sec * 1000);
-    });
+    
+
+    const q = await channel.assertQueue(queueName, { exclusive: true });
+    channel.prefetch(1)
+console.log("requesting fib",number)
+  // channel.sendToQueue(queueName,Buffer.from(number.toString()),{
+  //   replyTo:q.queue,
+
+  // })
+    channel.consume(
+      q.queue,
+      (message) => {
+        const number=parseInt(message.content.toString())
+        console.log("fib number",number)
+        const fib=fibonacci(number)
+
+      },
+      { noAck: true }
+    );
 
     console.log("Channel created successfully");
   } catch (error) {
     console.error("Error creating channel:", error);
   }
 }
-
 createChannel();
--- publisher/index.js
+++ publisher/index.js
@@ -1,24 +1,153 @@
 const amqp = require("amqplib");
+const { v4: uuidv4 } = require('uuid');
+// this example for normal queue without using exchange
+//  it diredctly send queue
+// const message = process.argv.splice(2).join(" ") || "hello world";
 
-const message = process.argv.splice(2).join(" ") || "hello world";
+// async function createChannel() {
+//   try {
+//     const connection = await amqp.connect("amqp://localhost");
+
+//     const channel = await connection.createChannel();
+//     const result = await channel.assertQueue("task", { durable: true });
+//     channel.sendToQueue("task", Buffer.from(message), { persistent: true });
+//     console.log("task sent succesfullt", message);
+
+//     console.log("Channel created successfully");
+//     setTimeout(() => {
+//       connection.close();
+//       process.exit(0);
+//     }, 500);
+//   } catch (error) {
+//     console.error("Error creating channel:", error);
+//   }
+// }
+
+// fanout method
+// const message = process.argv.splice(2).join(" ") || "hello world";
+// const exchangeName = "logs";
+// async function createChannel() {
+//   try {
+//     const connection = await amqp.connect("amqp://localhost");
+
+//     const channel = await connection.createChannel();
+//     const result = await channel.assertExchange(exchangeName, "fanout", {
+//       durable: false,
+//     });
+//     channel.publish(exchangeName, "", Buffer.from(message));
+//     console.log("task sent succesfullt", message);
+
+//     console.log("Channel created successfully");
+//     setTimeout(() => {
+//       connection.close();
+//       process.exit(0);
+//     }, 500);
+//   } catch (error) {
+//     console.error("Error creating channel:", error);
+//   }
+// }
+
+// direct exchange
+// const args = process.argv.slice(2);
+// const message = args[1] || "hello world";
+// const logType = args[0];
+// const exchangeName = "direct_logs";
+
+// async function createChannel() {
+//   try {
+//     const connection = await amqp.connect("amqp://localhost");
+
+//     const channel = await connection.createChannel();
+//     const result = await channel.assertExchange(exchangeName, "direct", {
+//       durable: false,
+//     });
+//     channel.publish(exchangeName, logType, Buffer.from(message));
+//     console.log("task sent succesfullt", message);
+
+//     console.log("Channel created successfully");
+//     setTimeout(() => {
+//       connection.close();
+//       process.exit(0);
+//     }, 500);
+//   } catch (error) {
+//     console.error("Error creating channel:", error);
+//   }
+// }
+
+// topic exchange
+
+// const args = process.argv.slice(2);
+// const message = args[1] || "hello world";
+// const logType = args[0];
+// const exchangeName = "topic_logs";
+
+// async function createChannel() {
+//   try {
+//     const connection = await amqp.connect("amqp://localhost");
+
+//     const channel = await connection.createChannel();
+//     const result = await channel.assertExchange(exchangeName, "topic", {
+//       durable: false,
+//     });
+//     channel.publish(exchangeName, logType, Buffer.from(message));
+//     console.log("msg sent succesfullt", message);
+
+//     console.log("Channel created successfully");
+//     setTimeout(() => {
+//       connection.close();
+//       process.exit(0);
+//     }, 500);
+//   } catch (error) {
+//     console.error("Error creating channel:", error);
+//   }
+// }
+
+// //  example for rpc-remote procedural protocall
+
+const args = process.argv.slice(2);
+const queueName="rpc_queue"
+
+if (args.length === 0) {
+  console.log("please enter the number");
+  process.exit(1);
+}
+const number=parseInt(args[0]); 
+const id=uuidv4()
 
 async function createChannel() {
+
   try {
+    // Connect to RabbitMQ server
     const connection = await amqp.connect("amqp://localhost");
 
+    // Create a channel
     const channel = await connection.createChannel();
-    const result = await channel.assertQueue("task", { durable: true });
-    channel.sendToQueue("task", Buffer.from(message), { persistent: true });
-    console.log("task sent succesfullt", message);
+    
+
+    const q = await channel.assertQueue("", { exclusive: true });
+console.log("requesting fib",number)
+  channel.sendToQueue(queueName,Buffer.from(number.toString()),{
+    replyTo:q.queue,
+   correlationId:id
+  })
+    channel.consume(
+      q.queue,
+      (message) => {
+        if (message.content) {
+          console.log(
+            "routing key :",
+            message.fields.routingKey,
+            "content : ",
+            message.content.toString()
+          );
+        }
+      },
+      { noAck: true }
+    );
 
     console.log("Channel created successfully");
-    setTimeout(() => {
-      connection.close();
-      process.exit(0);
-    }, 500);
   } catch (error) {
     console.error("Error creating channel:", error);
   }
 }
-
-createChannel();
+// createChannel();
